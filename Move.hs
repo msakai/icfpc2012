136c25a3452c382b946fe2059c493c951cde9e14
Masahiro Sakai <masahiro.sakai@gmail.com>
2012-07-14 01:25:07 +0900

    introduce GameState type

--- Move.hs
+++ Move.hs
@@ -14,34 +14,49 @@ data Move
   | A
   deriving (Ord, Eq, Show)
 
-move :: Move -> (Map, Pos) -> Maybe (Map, Pos)
-move move (m,(x,y)) =
+data GameState
+  = GameState
+  { gPos    :: !Pos
+  , gMap    :: !Map
+  , gScore  :: !Int
+  , gLambda :: !Int
+  , gSteps  :: !Int
+  }
+  deriving (Eq, Show)
+
+move :: Move -> GameState -> Maybe GameState
+move move s@GameState{ gMap = m, gPos = (x,y) } =
   case move of
     L -> f (x-1, y)
     R -> f (x+1, y)
     U -> f (x, y+1)
     D -> f (x, y-1)
-    W -> Just (m, (x,y))
+    W -> Just s
     A -> Nothing
   where
     f (x',y') = 
       case m ! (x',y') of
-        Empty          -> Just (m', (x',y'))
-        Earth          -> Just (m', (x',y'))
-        Lambda         -> Just (m', (x',y'))
-        OpenLambdaLift -> Just (m', (x',y'))
+        Empty          -> Just $ s'
+        Earth          -> Just $ s'
+        Lambda         -> Just $ s'{ gScore = gScore s' + 25 }
+        OpenLambdaLift -> Just $ s' -- TODO: Winning Condition
         Rock
           | x'==x+1 && y'==y &&
             inRange (bounds m) (x+2,y) &&  m ! (x+2,y) == Empty ->
               -- Additionally, the Rock moves to (x+2,y).
-              Just (m' // [((x+2,y), Rock)],  (x',y'))
+              Just $ s'{ gMap = gMap s' // [((x+2,y), Rock)] }
           | x'==x-1 && y'==y &&
             inRange (bounds m) (x-2,y) &&  m ! (x-2,y) == Empty ->
-              -- Additionally, the Rock moves to (x−2,y).
-              Just (m' // [((x-2,y), Rock)],  (x',y'))
-        _ -> Just (m, (x,y)) -- invalid
+              -- Additionally, the Rock moves to (x-2,y).
+              Just $ s'{ gMap = gMap s' // [((x-2,y), Rock)] }
+        _ -> Just s -- invalid
       where
-        m' = m // [((x,y),Empty), ((x',y'), Robot)]    
+        m' = m // [((x,y), Empty), ((x',y'), Robot)]    
+        s' = s{ gMap = m'
+              , gPos = (x',y')
+              , gScore = gScore s - 1
+              , gSteps = gSteps s + 1
+              }
 
 isValidMove :: Map -> Pos -> Pos -> Bool
 isValidMove m (x,y) (x',y') =
