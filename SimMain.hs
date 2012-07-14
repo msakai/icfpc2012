57d757edda2e47fe1a9df45c9c1eec5b0ea6ee65
Masahiro Sakai <masahiro.sakai@gmail.com>
2012-07-14 12:48:08 +0900

    add 'sim' command for interactive simulation

---
+++ SimMain.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import System.Environment
+import System.Exit
+import System.IO
+
+import Map
+import Move
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [fname] -> do
+      s <- readFile fname
+      let m = parseMap s
+      interactiveSim m
+    _ -> do
+      hPutStrLn stderr "Usage: sim file.map"
+      exitFailure
