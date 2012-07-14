4a3dc26ee55f418e6563f63c8d70f82dd2648d25
Masahiro Sakai <masahiro.sakai@gmail.com>
2012-07-14 23:06:10 +0900

    split random walker into RandomWalk.hs

---
+++ src/RandomWalk.hs
@@ -0,0 +1,40 @@
+{-# OPTIONS_GHC -Wall #-}
+module RandomWalk
+  ( run
+  ) where
+
+import Control.Monad
+import Data.Array
+import Data.IORef
+import qualified System.Random as Rand
+
+import Move
+import Sim
+
+run :: IORef ([Command], Int) -> GameState -> IO ()
+run bestRef s0 = forever $ walk s0 []
+  where
+    stepLim = rangeSize $ bounds $ gMap s0
+
+    walk :: GameState -> [Command] -> IO ()
+    walk s cmds = do
+      if gSteps s > stepLim
+        then return ()
+        else do
+          check (step s A) (A : cmds)
+          c <- randomCommand
+          walk (step s c) (c : cmds)
+
+    check :: GameState -> [Command] -> IO ()
+    check s cmds = do
+      (_, bestScore) <- readIORef bestRef
+      let score = gScore s
+      if score > bestScore
+        then writeIORef bestRef (cmds, score)
+        else return ()
+
+randomCommand :: IO Command
+randomCommand = do
+  let cmds = [L,R,U,D,W] -- Aは無条件にチェックするのでここで候補にはしない
+  i <- Rand.getStdRandom $ Rand.randomR (0, length cmds - 1)
+  return $ cmds !! i
--- src/Solver.hs
+++ src/Solver.hs
@@ -1,50 +1,20 @@
+{-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Solver where
 
 import Control.Exception
-import Control.Monad
-import Data.Array
 import Data.IORef
-import qualified System.Random as Rand
 
 import Move
 import Sim
+import qualified RandomWalk
 
 run :: GameState -> IO ([Command], Int)
 run s = do
   bestRef <- newIORef ([A], 0)  
-  result <- try $ (run' bestRef s)
+  result <- try $ RandomWalk.run bestRef s
   case result of
     Right () -> return ()
-    Left (e :: AsyncException) -> return ()
+    Left (_ :: AsyncException) -> return ()
   (cmds, score) <- readIORef bestRef
   return (reverse cmds, score)
-
-run' :: IORef ([Command], Int) -> GameState -> IO ()
-run' bestRef s = forever $ walk s []
-  where
-    stepLim = rangeSize $ bounds $ gMap s
-
-    walk :: GameState -> [Command] -> IO ()
-    walk s cmds = do
-      if gSteps s > stepLim
-        then return ()
-        else do
-          check s cmds
-          check (step s A) (A : cmds)
-          c <- randomCommand
-          walk (step s c) (c : cmds)
-
-    check :: GameState -> [Command] -> IO ()
-    check s cmds = do
-      (bestCmds, bestScore) <- readIORef bestRef
-      let score = gScore s
-      if score > bestScore
-        then writeIORef bestRef (cmds, score)
-        else return ()
-
-randomCommand :: IO Command
-randomCommand = do
-  let cmds = [L,R,U,D,W]
-  i <- Rand.getStdRandom $ Rand.randomR (0, length cmds - 1)
-  return $ cmds !! i
