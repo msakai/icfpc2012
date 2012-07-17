52f0e65b3ee50d5a77559d4d3624f1e54acb47f1
Masahiro Sakai <masahiro.sakai@gmail.com>
2012-07-18 01:21:07 +0900

    add --algorithm option to 'lifter'

--- src/LifterMain.hs
+++ src/LifterMain.hs
@@ -1,16 +1,42 @@
 module Main where
 
 import Data.Char
+import Data.List
+import System.Console.GetOpt
 import System.Environment
+import System.Exit
+import System.IO
 
 import Move
 import Sim
 import Solver 
 
+data Options
+  = Algo String
+
+header :: String -> String
+header prog = "Usage: " ++ prog ++ " [--algorithm=str] < file.map"
+
+options :: [OptDescr Options]
+options =
+  [ Option ['a'] ["algorithm"] (ReqArg Algo "<algorithm>")
+      ("algorithm: " ++ intercalate ", " [name | (name,_) <- algorithms])
+  ]
+
 main :: IO ()
 main = do
   prog <- getProgName
-  str <- getContents
-  let s = initialStateFromString str
-  (cmds, score) <- Solver.run s (takeWhile isAlphaNum prog)
-  putStrLn $ showCommands cmds
+  let algo0 = takeWhile isAlphaNum prog
+
+  args <- getArgs
+  case getOpt Permute options args of
+    (o,args2,[]) -> do
+      let algo = last $ algo0 : [name | Algo name <- o]
+
+      str <- getContents
+      let s = initialStateFromString str
+      (cmds, score) <- Solver.run s algo
+      putStrLn $ showCommands cmds
+    _ -> do
+      hPutStrLn stderr (usageInfo (header prog) options)
+      exitFailure
